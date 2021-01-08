eff363c3159d4f2080a2ef0d886fee04db10f6a3
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-01-08 16:11:15 +0900

    로그인페이지 추가

--- application/views/kor/login.php
+++ application/views/kor/login.php
@@ -1,16 +1,20 @@
-
+<html>
+    <head>
+        <title>Refobi</title>
+    </head>
+    
+    <body>
 	<div class="container">
-		<div class="page-header">
-			<h1>테스트 화면</h1>
-			<p class="lead">현재 보시고 계신 페이지는 테스트 페이지 입니다..</p>
-		</div>
 
-
-		<h3>시범메뉴리스트</h3>
 		<div class="row">
-			<div class="col-xs-4">작업중</div>
-			<div class="col-xs-4">작업중</div>
-			<div class="col-xs-4">작업중</div>
+			<div class="col-xs-4"></div>
+                        <div class="col-xs-4"><h3>로그인</h3></div>
+			<div class="col-xs-4"></div>
+		</div>
+                <div class="row">
+			<div class="col-xs-4"></div>
+			<div class="col-xs-4">로그인</div>
+			<div class="col-xs-4"></div>
 		</div>
 	</div>
 </body>
