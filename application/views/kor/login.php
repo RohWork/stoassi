86a994092e7344ecfc127d3313d2e1d84aea6454
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-01-08 16:15:10 +0900

    로그인페이지 추가

--- application/views/kor/login.php
+++ application/views/kor/login.php
@@ -7,9 +7,9 @@
 	<div class="container">
 
 		<div class="row">
-			<div class="col-xs-4"></div>
+			<div class="col-xs-4">&nbsp;</div>
                         <div class="col-xs-4"><h3>로그인</h3></div>
-			<div class="col-xs-4"></div>
+			<div class="col-xs-4">&nbsp;</div>
 		</div>
                 <div class="row">
 			<div class="col-xs-4"></div>
