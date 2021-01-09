5ef89e6847bc655f068ed0ac9459026be2e5a566
ROH <ROH@125.133.43.177>
2021-01-09 20:08:21 +0900

    로그인 화면수정
    요청자 : 노진국

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://www.refobi.com';
+$config['base_url'] = 'http://www.stoassi.com';
 
 /*
 |--------------------------------------------------------------------------
