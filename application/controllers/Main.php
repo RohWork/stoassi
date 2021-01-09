8b00005ce4dbeb766639df71deebfaf3c940cf88
ROH <ROH@125.133.43.177>
2021-01-09 20:08:31 +0900

    로그인 화면수정
    요청자 : 노진국

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -19,6 +19,16 @@ class Main extends CI_Controller {
 		$this->load->view(LANGUAGE.'/login');
 	}
         
+        public function login_process()
+        {
+            $data = array(
+                "test" => "test",
+            );
+                    
+            header("Content-Type: application/json;");
+            echo json_encode($data);
+        }
+        
         public function main(){
                 $this->load->view(LANGUAGE.'/header', $this->head_data);
 		$this->load->view(LANGUAGE.'/main');
