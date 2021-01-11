a243dbf97cc3a8a36a12a3e58647633542e4a93d
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-01-11 15:47:21 +0900

    로그인페이지 추가

--- application/config/config.php
+++ application/config/config.php
@@ -367,8 +367,8 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'databases';
-$config['sess_cookie_name'] = 'ci_session';
+$config['sess_driver'] = 'database';
+$config['sess_cookie_name'] = 'ci_sessions';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
