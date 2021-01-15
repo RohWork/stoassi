764600282f866e51f89ff2bd5d84fb8f0032434d
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-01-15 16:56:26 +0900

    hooks 설정및 header수정
    수정자 : 노진국

--- application/hooks/Log.php
+++ application/hooks/Log.php
@@ -1,17 +1,28 @@
 <?php
 class Log extends CI_Controller{
     
-
+    private $CI;
+    
+    function __construct()
+    {
+        $this->CI =& get_instance();
+        
+         
+        if(!isset($this->CI->session)){  
+              $this->CI->load->library('session');  
+        }
+    }
     
     function checkPermission(){
        
+        $CI =& get_instance();
 
-        $this->load->helper('url');
+        $CI->load->helper('url');
        
-        if(isset($this->allow) && (is_array($this->allow) === false OR in_array($this->router->method, $this->allow) === false))
+        if(isset($CI->allow) && (is_array($CI->allow) === false OR in_array($CI->router->method, $CI->allow) === false))
         {
 
-            if (!$this->session->userdata('user_id')) // 로그인 여부를 세션을 이용해 체크한다.
+            if (!$CI->session->userdata('user_id')) // 로그인 여부를 세션을 이용해 체크한다.
             {
                 
                 echo "test";
