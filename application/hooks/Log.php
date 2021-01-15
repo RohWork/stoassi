7f0ed678cceaa2fd12571a882d4b0c10a0b02ce6
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-01-15 15:15:58 +0900

    hooks 설정및 header수정
    수정자 : 노진국

--- application/hooks/Log.php
+++ application/hooks/Log.php
@@ -24,7 +24,7 @@ class Log {
         if(isset($CI->allow) && (is_array($CI->allow) === false OR in_array($CI->router->method, $CI->allow) === false))
 // 여기에 쓰인 if문을 이해하는데 한참 걸렸습니다..
 // $CI->allow 는 배열로, 추후 로그인 없이 사용 가능한 것들을 저장할때 사용합니다.
-// 가령 로그인 화면이나 문의화면 같이 비로그인으로 열어줄 것들을 넣어주게 됩니다.
+// 가령 로그인 화면이나 문의화면 같이 비로그인으로 열어줄 것들을 넣어주게 됩니다. 
         {
             if (!$CI->session->userdata('user_id')) // 로그인 여부를 세션을 이용해 체크한다.
             {
