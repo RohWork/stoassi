790b1b2ee249948c0a75d34b50d46367bbf74455
ROH <ROH@125.133.43.177>
2021-01-31 00:33:07 +0900

    회원가입승인 추가
    요청자 : 노진국

--- application/models/Member_model.php
+++ application/models/Member_model.php
@@ -27,6 +27,23 @@ class Member_model extends CI_Model {
         return $this->db->count_all_results();
     }
     
+    function get_member_list($params){
+        
+        $this->db->select('mi.idx, mi.shop_idx, mi.id, mi.NAME AS user_name, mi.tel, mi.LEVEL');
+        $this->db->select('mi.email, si.addr, si.name AS shop_name, sc.name AS shop_category ');
+        $this->db->from('member_info', 'mi');
+        $this->db->join('shop_info as si', 'mi.shop_idx = si.idx', 'left');
+        $this->db->join('shop_category as sc', 'si.category_idx = sc.idx', 'left');
+        
+        if($params['user_id']){
+            $this->db->where('mi.id', $params['user_id']);
+        }
+        
+        $this->db->order_by("mi.idx", "desc");
+        $this->db->limit($search_vo->config_per_page, $offset);
+        
+        return $this->db->result();
+    }
     function set_member($data){
         
         $data['regi_date'] = date('Y-m-d H:i:s');
