47a67d907d5c2d7042f0de400b16eb7b9c999b38
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-02-03 10:11:03 +0900

    LMS 다이아몰 입금대기에서 결제 완료로 수정시 상단으로 올라오게 등록일 재수정
    요청자 : 김양지
    
    수정자 : 노진국

--- application/models/Member_model.php
+++ application/models/Member_model.php
@@ -14,6 +14,18 @@ class Member_model extends CI_Model {
         
         return $this->db->get()->row_array();
     }
+    function get_member_info_idx($user_idx){
+        
+        $this->db->select('mi.idx, mi.shop_idx, mi.id, mi.NAME AS user_name, mi.tel, mi.LEVEL');
+        $this->db->select('mi.email, si.addr, si.name AS shop_name, sc.name AS shop_category ');
+        $this->db->from('member_info as mi');
+        $this->db->join('shop_info as si', 'mi.shop_idx = si.idx', 'left');
+        $this->db->join('shop_category as sc', 'si.category_idx = sc.idx', 'left');
+        $this->db->where('mi.idx', $user_idx);
+        
+        return $this->db->get()->row_array();
+    
+    }
     
     function count_member_list($search_vo){
         
