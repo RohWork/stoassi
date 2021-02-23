b5d9f19c6ad4174570db1af94892b0b102399765
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-02-23 14:21:03 +0900

    레시피관리
    수정자 : 노진국

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -95,7 +95,9 @@ if ( ! function_exists('header_set')) {
             "main"	=> "",
             "stock_list" => "",
             "stock_category" => "",
-
+            "stock_seller" => "",
+            "stock_history" => "",
+            "recipe_group"   => "",
             "recipe_list"   => "",
             "stock" => $stock,
             "recipe"    => $recipe
