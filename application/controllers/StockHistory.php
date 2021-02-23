f750f3f792400a4b1967d3cbc519787fe3f29664
soonsoo_ict <soonsoo_ict@DESKTOP-DONSES6>
2021-02-23 14:24:06 +0900

    레시피관리
    수정자 : 노진국

---
+++ application/controllers/StockHistory.php
@@ -0,0 +1,20 @@
+<?php
+class StockHistory extends CI_Controller {
+	
+	function __construct() {
+            parent ::__construct();
+            
+            $this->head_data = header_set("stock_history");
+
+            $this->load->model('Recipe_model', 'recipe_md', TRUE);
+            
+        }
+
+        function index(){
+            
+            
+            
+            $this->load->view(LANGUAGE.'/header', $this->head_data);
+        }
+        
+}
