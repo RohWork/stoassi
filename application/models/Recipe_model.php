8e8290b4d11a28df32cebae64ec7a99202a149aa
ROH <ROH@125.133.43.177>
2021-02-27 08:03:28 +0900

    회원가입승인 추가
    요청자 : 노진국

--- application/models/Recipe_model.php
+++ application/models/Recipe_model.php
@@ -6,6 +6,27 @@ class Recipe_model extends CI_Model {
         $this->load->database('default', TRUE);
     }
     
-    
+    function count_group_list($search_vo){
+        
+        $this->db->select('*');
+        $this->db->from('recipe_group as rg');
+        $this->db->where('rg.shop_idx', $search_vo->shop_idx);
+        $this->db->order_by("rg.idx", "desc");
+        
+        return $this->db->count_all_results();
+        
+    }
+
+    function get_group_list($offset, $search_vo){
+        
+        $this->db->select('*');
+        $this->db->from('recipe_group as rg');
+        $this->db->where('rg.shop_idx', $search_vo->shop_idx);
+        $this->db->order_by("rg.idx", "desc");
+        $this->db->limit($search_vo->config_per_page, $offset);
+        
+        return $this->db->get()->result();
+        
+    }	
 }    
 ?>
