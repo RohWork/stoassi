587abfb81c67795cf7c3fe29f849e7bfa0431fc4
ROH <ROH@125.133.43.177>
2021-02-27 08:41:06 +0900

    회원가입승인 추가
    요청자 : 노진국

--- application/models/Recipe_model.php
+++ application/models/Recipe_model.php
@@ -47,5 +47,14 @@ class Recipe_model extends CI_Model {
         
         
     }
+    
+    function category_update($data, $idx){
+
+        $data['modi_date'] = date('Y-m-d H:i:s');
+        $data['modifier'] = $this->session->userdata("user_id");
+        
+        $this->db->where("idx",$idx);
+        $this->db->update('stock_category',$data);
+    }
 }    
 ?>
