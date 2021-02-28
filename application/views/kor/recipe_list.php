b5339344e0ac47ca13147b609e5c2abc4e4b4f20
ROH <ROH@125.133.43.177>
2021-02-28 10:14:04 +0900

    회원가입승인 추가
    요청자 : 노진국

--- application/views/kor/recipe_list.php
+++ application/views/kor/recipe_list.php
@@ -41,7 +41,7 @@
 			</table>
 		</div>
 		<div class="col-sm-12">
-			<button type="button" id="input_button" class="btn btn-primary">그룹 추가</button>
+			<button type="button" id="input_button" class="btn btn-primary">레시피 추가</button>
 		</div>
 		<div class="col-sm-offset-5">
 			<ul class="pagination">
