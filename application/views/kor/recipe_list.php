e8381d2b737403253b174a5292d910ff34248c24
ROH <ROH@125.133.43.177>
2021-02-28 10:30:48 +0900

    레시피관리추가
    요청자 : 노진국

--- application/views/kor/recipe_list.php
+++ application/views/kor/recipe_list.php
@@ -4,8 +4,8 @@
 			<h1>레시피 관리</h1>
 			<p class="lead">레시피 관리 화면</p>
 		</div>
-                <div class="row">
-                    <div class="col-sm-2 mb-2" >
+                <div class="row mb-2">
+                    <div class="col-sm-2" >
                         <select id="group_select" name="group_select" class="form-control">
                             <?php
                                 foreach($group_rows as $grow){
